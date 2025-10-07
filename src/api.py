4366f30c8c1a32b11e2ec61d8192cab1124b60ec
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2025-10-07 08:25:20 +0300

    Add private to api

--- src/api.py
+++ src/api.py
@@ -6,6 +6,13 @@ import requests
 class AbstractHeadHunterAPI(ABC):
     """ Абстрактный класс AbstractHeadHunterAPI """
 
+    @staticmethod
+    @abstractmethod
+    def validate_status_cod(response: requests.models.Response) -> bool:
+        """ Абстрактный метод проверки ответа сервера """
+
+        pass
+
     @abstractmethod
     def load_vacancies(self, keyword: str) -> None:
         """ Абстрактный метод загрузки вакансий"""
@@ -22,12 +29,22 @@ class HeadHunterAPI(AbstractHeadHunterAPI):
         self.params = {'text': '', 'page': 0, 'per_page': 100}
         self.vacancies = []
 
-    def load_vacancies(self, keyword: str) -> None:
+    @staticmethod
+    def validate_status_cod(response: requests.models.Response) -> bool:
+        """ Метод проверки ответа сервера """
+
+        if response.status_code != 200:
+            print(f"status_cod {response.status_code}")
+            return True
+
+    def __load_vacancies(self, keyword: str) -> None:
         """ Метод загрузки вакансий """
 
         self.params['text'] = keyword
         while self.params.get('page') != 20:
             response = requests.get(self.url, headers=self.headers, params=self.params)
+            if self.validate_status_cod(response):
+                break
             vacancies = response.json()['items']
             self.vacancies.extend(vacancies)
             self.params['page'] += 1
